98cc8849dc39e2b1eebdd9da0a90cbac2a42155e
Andrei Luculescu <andrei-alexandru.luculescu.ext@siemens-energy.com>
2024-08-03 12:34:09 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+// Add your imported modules here
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const main = async (): Promise<void> => {
+    // start your app here
+};
+
+main();
